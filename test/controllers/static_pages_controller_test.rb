cc6a8334d493a034b86d60c05fe2b12395da6030
Mauricio Robayo <rfmajo@gmail.com>
2019-09-09 12:55:13 -0500

    Initial layout

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
+  test "should get catch all" do
+    get '/' << ('a'..'z').to_a.sample(10).join
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+end
